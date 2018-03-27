58bb33edc97051a6b0cbddc0865adc775afb6d7c
Joshua Burkhow <joshua@dataenthusiast.com>
2018-03-27 08:25:26 -0400

    Update Install.ps1
    I needed to update these two lines in order to get it to install appropriately ;)

--- Scripts/Install.ps1
+++ Scripts/Install.ps1
@@ -1,8 +1,8 @@
 ﻿$root = Split-Path -Parent $PSCommandPath
 Push-Location $root
-.\Installer.ps1 "OmniBus" "OmniBus" "..\OmniBus"
+.\Installer.ps1 "OmniBus" "OmniBus" "..\AlteryxAddIns"
 .\Installer.ps1 "OmniBus.XmlTools" "OmniBus" "..\OmniBus.XmlTools"
-.\Installer.ps1 "OmniBus.Roslyn" "OmniBus" "..\OmniBus.Roslyn"
+.\Installer.ps1 "OmniBus.Roslyn" "OmniBus" "..\AlteryxAddIns.Roslyn"
 .\InstallerHTML.ps1 "..\OmniBusRegex"
 .\Uninstaller.ps1 "JDTools"
 .\Uninstaller.ps1 "RoslynPlugIn"
